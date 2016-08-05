91ca12074631ac07d95088af3eac9ec9eaa17c3b
JinWYP <jinwyp@gmail.com>
2016-08-05 09:19:32 +0800

    update

--- backend/src/.eslintrc.js
+++ backend/src/.eslintrc.js
@@ -6,22 +6,10 @@ module.exports = {
     "extends": "eslint:recommended",
     "rules": {
         "no-console":0,
-
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
+        "indent"          : ["error", 4],
+        "linebreak-style" : ["error", "unix"],
+        "quotes"          : ["error", "single"],
+        "semi"            : ["error", "always"],
+        "comma-dangle"    : ["error", "never"]
     }
 };
